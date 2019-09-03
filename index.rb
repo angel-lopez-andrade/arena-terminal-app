f575098f4a386d924acef0a39b8b914f088fa634
poklipart <angellopezandrade4@gmail.com>
2019-09-03 11:27:13 +1000

    added command line argument checker to index file

--- index.rb
+++ index.rb
@@ -1,1 +1,12 @@
 require "colorize"
+
+if ARGV.length != 1
+    puts "Wrong number of arguments."
+    puts "Exactly one argument - player name, required at runtime."
+    puts "Exiting application."
+    exit
+elsif ARGV[0].length < 3 or ARGV[0].length > 9
+    puts "Player name too long or too short."
+    puts "Please input a name between 3 and 9 characters"
+    puts "Exiting application."
+end
